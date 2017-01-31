c3b3a170f3114696d7ede2d0a0ffecae979eb5ce
RustyRadio <danielc@my.ccsu.edu>
2017-01-31 16:40:15 -0500

    Okay reverting, mobile wont have bombs for right now, will have to make a bomb box

--- main.js
+++ main.js
@@ -246,13 +246,7 @@ var compareShots = function(x2, y2, radius) {
 }
 $(document).ready(function() {
     $('body').append(c);
-    alert("update .07");
-    $(document).on('taphold', 'canvas', function(e) {
-        alert("jj") ;  
-        e.preventDefault();
-    } );
-    $(document).on('touchend', 'canvas', function(e) {
-        alert("kk2 " + e.originalEvent.touches[0].pageX);
+    $(document).on('touchstart', 'canvas', function(e) {
         e.preventDefault();
         mobileHelp = 5;
         clickEvent(e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY);
