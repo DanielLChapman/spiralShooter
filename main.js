3d76e8efdb03b68278e976c05e1aba1c75f97727
RustyRadio <danielc@my.ccsu.edu>
2017-01-31 16:34:26 -0500

    Okay, this should work

--- main.js
+++ main.js
@@ -246,12 +246,12 @@ var compareShots = function(x2, y2, radius) {
 }
 $(document).ready(function() {
     $('body').append(c);
-    alert("update .04");
+    alert("update .05");
     $('canvas').bind('taphold', function(e) {
         alert("jj") ;  
         e.preventDefault();
     } );
-    $(document).on('tap', 'canvas', function(e) {
+    $(document).on('touchend', 'canvas', function(e) {
         e.preventDefault();
         mobileHelp = 5;
         clickEvent(e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY);
